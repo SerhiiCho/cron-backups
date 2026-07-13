73afee7d68f4df828b608e3c815795d0773b770e
SerhiiCho <serhiicho@protonmail.com>
2026-07-13 12:38:50 +0300

    chore: wip on deleting old files script

--- lib/files.sh
+++ lib/files.sh
@@ -1,15 +1,16 @@
-copy_files_from_remote() (
-    log_file="$(date '+%Y-%m-%d.log')"
+log_file="logs/$(date '+%Y-%m-%d.log')"
 
+copy_files_from_remote() (
     echo "🐧 Copying SQL backups..."
     rsync -avz --timeout=30 "$REMOTE_VPS_HOST":"$SQL_BACKUPS_FROM_PATH" \
-        backups >> logs/"$log_file" 2>&1
+        backups >> "$log_file" 2>&1
 
     echo "🐧 Copying storage files..."
     rsync -avz --timeout=30 "$REMOTE_VPS_HOST":"$STORAGE_BACKUPS_FROM_PATH" \
-        storage >> logs/"$log_file" 2>&1
+        storage >> "$log_file" 2>&1
 )
 
 cleanup_old_local_files() (
-    echo "🐧 Old files are deleted"
+    printf "🐧 Deleting log files older than %d days\n"
+    find logs -type f -mtime +20 ! -name '.gitignore'
 )
